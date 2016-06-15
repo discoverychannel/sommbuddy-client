298c26ba45958da4903da8579a7525cbf0bb4e0c
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-06-15 16:56:55 -0700

    corrected orphan curly

--- www/js/service.js
+++ www/js/service.js
@@ -15,38 +15,40 @@
          console.log(userSelection);
          return userSelection
       },
-      // getWines: function(price) {
-      //   $http.get('filters.json').then(function(data){
-      //     for(filters in data){
-      //       if(filters.val == userSelection){
-      //         return filters.filterCodes[Math.floor(Math.random()*filters.filterCodes.length)];
-      //       }
-      //     }
-      //   })
-        // .then(function(filterCode) {
-        //   $http.get('/filterCode' + price).then(function(d) {
-        //     function Wine (name, grape, vineyard, vintage, region, price, picture){
-        //       this.name = name,
-        //       this.grape = grape,
-        //       this.vineyard = vineyard,
-        //       this.vintage = vintage,
-        //       this.region = region,
-        //       this.price = price,
-        //       this.picture = picture,
-        //     }
-        //
-        //     var wine1 = new Wine(
-        //       d.first.name,
-        //       d.first.grape,
-        //       d.first.vineyard,
-        //       d.first.vintage,
-        //       d.first.region,
-        //       d.first.price,
-        //       d.first.picture,
-        //     );
-        //   })
-        // })
-      // }
+      getWines: function(price) {
+        $http.get('filters.json').then(function(data){
+          for(filters in data){
+            if(filters.val == userSelection){
+              return filters.filterCodes.join('+');
+              // "123+235+4547"
+              // return filters.filterCodes[Math.floor(Math.random()*filters.filterCodes.length)];
+            }
+          }
+        })
+        .then(function(filterCode) {
+          $http.get('/' + price + filterCodes).then(function(d) {
+            function Wine (name, grape, vineyard, vintage, region, price, picture){
+              this.name = name;
+              this.grape = grape;
+              this.vineyard = vineyard;
+              this.vintage = vintage;
+              this.region = region;
+              this.price = price;
+              this.picture = picture;
+            }
+
+            var wine1 = new Wine(
+              d[0].Name,
+              d[0].Varietal.Name,
+              d[0].Vineyard.Name,
+              d[0].Vintage,
+              d[0].Appellation.Name + ', ' + d[0].Apellation.Region.Name,
+              d[0].Price,
+              d[0].Picture
+            );
+          })
+        })
+      }
     }
   }
 })();
