784051f48c11202cad2e4bf4bf868280ab4cde86
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-06-15 23:04:02 -0700

    builds url with price and grape codes

--- www/js/service.js
+++ www/js/service.js
@@ -16,18 +16,20 @@
          return userSelection;
       },
       getWines: function(price) {
+        var code;
         $http.get('./js/filters.json').then(function(data){
           var filterObjs = JSON.parse(JSON.stringify(data)).data;
           filterObjs.forEach(function(filterObj){
             console.log(filterObj);
             if(filterObj.val == userSelection){
-              console.log(filterObj.filterCodes.join('+'));
-              return filterObj.filterCodes.join('+');
+              code = filterObj.filterCodes.join('+');
+              console.log(code);
             }
           })
+          return price + code;
         })
         .then(function(filterCodes) {
-          $http.get('/' + price + filterCodes).then(function(d) {
+          $http.get('/' + filterCodes).then(function(d) {
             function Wine (name, grape, vineyard, vintage, region, price, picture){
               this.name = name;
               this.grape = grape;
