2b69b00a0078e6f812b6010d70ae47351e2dbb21
austinsprague <sprague.ak@gmail.com>
2016-06-17 11:35:34 -0700

    like only one

--- www/js/resultscontroller.js
+++ www/js/resultscontroller.js
@@ -10,22 +10,18 @@
       Selections.getWines($stateParams.price).then(function(wines) {
         vm.results = wines;
         $ionicSlideBoxDelegate.update();
-        // console.log('wines', wines);
       })
-      vm.like = false;
-      vm.liked = function (wine) {
-        //check a method in loginService to see if user is logged in
 
-        //if logged in = true, save insertWine(wine)
-        vm.like = !vm.like;
-        if (vm.like) {
+      vm.liked = function (wine) {
+        wine.like = !wine.like;
+
+        if (wine.like) {
           Selections.insertWine(wine).then(function(item){
             vm.wineId = item.data[0];
           });
         } else {
           Selections.removeWine(wine).then(function(removed){
             return vm.removedQuantity = removed.data[0];
-            console.log(vm.removedQuantity);
           });
         }
       };
